4e5de1fe7d01872654782f957c60549af1a3b5ac
yenas0 <zzlm980@gmail.com>
2024-07-31 07:26:17 +0900

    gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,16 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 allprojects {
     repositories {
         google()
