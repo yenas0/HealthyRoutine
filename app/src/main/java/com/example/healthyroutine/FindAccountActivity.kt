c3278a235b659310c7c6f9a7e7bc840742155ddc
yenas0 <zzlm980@gmail.com>
2024-07-31 08:05:48 +0900

    id, pw 찾기

--- app/src/main/java/com/example/healthyroutine/FindAccountActivity.kt
+++ app/src/main/java/com/example/healthyroutine/FindAccountActivity.kt
@@ -8,6 +8,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.example.healthyroutine.R
 
 class FindAccountActivity : AppCompatActivity() {
 
