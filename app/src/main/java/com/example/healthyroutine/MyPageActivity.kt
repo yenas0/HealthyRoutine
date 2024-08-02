4809161f00f1948394f9fa1cef39ae3ce6e4424c
jiyeoniii <dlwldus0705@naver.com>
2024-08-02 12:01:21 +0900

    하단 메뉴바

--- app/src/main/java/com/example/healthyroutine/MyPageActivity.kt
+++ app/src/main/java/com/example/healthyroutine/MyPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.healthyroutine
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
